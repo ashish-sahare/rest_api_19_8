5bd2e96bb649d122cb229e53fe487f7c78be5f78
ashish-sahare <ashishsahare2000@gmail.com>
2021-08-23 11:24:37 +0530

    Add basic test for controller

--- test/controllers/words_controller_test.rb
+++ test/controllers/words_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class WordsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    RSpec.describe WordsController do
+    describe 'GET index' do
+      context 'when some words present' do
+        it 'assigns @words' do
+          word = Word.create
+          get :index
+          expect(assigns(:words)).to eq([word])
+        end
+      end
+
+      context 'when some words present' do
+        it 'assigns @words' do
+          word = Word.create
+          get :index
+          expect(assigns(:words)).to eq([word])
+        end
+      end
+
+
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
 end
